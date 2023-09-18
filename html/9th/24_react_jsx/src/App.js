99b216e183fe6e14c1dbe686d3a679bee15c6e77
kangseokjooo <zpckq69@naver.com>
2023-09-18 19:41:07 +0900

    React:jsx 개념 예제

--- html/9th/24_react_jsx/src/App.js
+++ html/9th/24_react_jsx/src/App.js
@@ -1,6 +1,23 @@
 import "./App.css";
 
 function App() {
+  //map함수
+  const lists=['k','d','t','w','e','b'];
+  //filter함수
+  const animals=['dog','cat','rabbit'];
+  const newAnimals=animals.filter((value)=>{
+    return value.includes('a')
+  })
+  console.log(newAnimals);
+  //단축평가
+  //&&연산자
+  const result=false && 'Hello'
+  console.log(result);
+  //||연산자
+  const defaultValue=1000
+  const price=null
+  const dbPrice=price||defaultValue;
+  console.log(dbPrice);
   return (
     <>
       {/*<img src={logo}></img>*/}
@@ -13,6 +30,11 @@ function App() {
         <div className="navy"></div>
         <div className="purple"></div>
       </div>
+      {lists.map((value,id)=>{
+        return <div key={id}>
+          <p>Hello {value}</p>
+        </div>
+      })}
     </>
   );
 }
