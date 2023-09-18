49a50eda16f6c4bbecf4625983ac609d56d22c47
kangseokjooo <zpckq69@naver.com>
2023-09-18 17:54:57 +0900

    React:JSX 기본 및 실습

---
+++ html/9th/24_react_jsx/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/*<img src={logo}></img>*/}
+      <div className="container">
+        <div className="red"></div>
+        <div className="orange"></div>
+        <div className="yellow"></div>
+        <div className="green"></div>
+        <div className="blue"></div>
+        <div className="navy"></div>
+        <div className="purple"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
