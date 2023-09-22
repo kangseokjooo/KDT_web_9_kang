60411d027b819be85ea126c63b52b366ead41a71
kangseokjooo <zpckq69@naver.com>
2023-09-22 18:35:00 +0900

    React:concept

--- html/9th/react_basic/src/App.js
+++ html/9th/react_basic/src/App.js
@@ -1,27 +1,11 @@
-//  import ClassComponent from "./ClassComponent";
-//  import  FunctionComponent  from "./FunctionComponent"; 
-// import FoodComponet from "./FoodComponent";
-// import BestsellerComponent from "./BestsellerComponent";
-// import Event from "./Event";
+// import Book from "./Book";
+import LifeCycleFunc from "./08Lifecycle/LifeCycleFunc";
 
-import EventClassTest from "./EventClassTest";
-
-// import EventClass from "./EventClass";
- function App() {
-  return (
-    <>
-      {/* <ClassComponent name={"kang"} age={10}></ClassComponent>
-      <ClassComponent/> */}
-      {/* <FunctionComponent myClass={'kdt9'}>coding</FunctionComponent>
-      <FunctionComponent/> */}
-      {/* <FoodComponet food={"피자"}/>
-      <FoodComponet/>
-      <BestsellerComponent title={"리액트"} author={"asdadas"} price={15000} type={"컴퓨터"}/> */}
-      {/* <Event/> */}
-      {/* <EventClass/> */}
-      <EventClassTest message={"안녕하세요"}/>
-    </>
-  )
+function App() {
+    return <>
+        <LifeCycleFunc/>
+    
+    </>;
 }
 
 export default App;
